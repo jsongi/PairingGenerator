b03396419d55dc0b9da20715becb69dbd99f0086
Jason G <42660969+jsongi@users.noreply.github.com>
2024-10-25 23:05:06 -0700

    Handling dropdown menu selection and refactoring window creation

--- view.py
+++ view.py
@@ -1,34 +1,57 @@
 import tkinter as tk
+from tkinter import font
 
-def newWindow(root):
+#Initialize main window
+root = tk.Tk()
+root.title("Pairing Generator")
+root.geometry("1280x720")
+
+root.columnconfigure(0, weight=1)
+root.columnconfigure(1, weight=2)
+
+def newWindow(root, choice):
     #hide the main window
     root.withdraw()
 
-    newWindow = tk.Toplevel(root)
-    newWindow.title("New Window")
-    newWindow.geometry("1280x720")
+    if choice != "Generate Groups":
+        addRemoveWindow(choice)
+    else:
+        generateGroupsWindow(choice)
 
-    label = tk.Label(newWindow, text="This is a new window!")
-    label.pack(pady=20)
+def addRemoveWindow(choice):
+    #Certain buttons remain constant, but their functions change, and output will differ
+    return #placeholder
 
-    back_button = tk.Button(newWindow, text="Go Back", command=lambda: exitWindow(root, newWindow))
-    back_button.pack()
+def generateGroupsWindow(choice):
+    return #placeholder
 
-def exitWindow(root, window):
-    window.destroy()
-    #show the main window again
-    root.deiconify()
+def handleSelection(choice):
+    match choice:
+        case "Add Person":
+            newWindow(choice)
+        case "Remove Person":
+            newWindow(choice)
+        case "Add As Organizer":
+            newWindow(choice)
+        case "Remove As Organizer":
+            newWindow(choice)
+        case "Generate Groups":
+            newWindow(choice)
+        case _:
+            print("ERROR")    
 
 def initializeView():
-    root = tk.Tk()
-    root.title("Pairing Generator")
-    root.geometry("1280x720")
-
     selected_option = tk.StringVar()
-    selected_option.set("Select an option") 
+    selected_option.set("Select Option") 
 
     options = ["Add Person", "Remove Person", "Add As Organizer", "Remove As Organizer", "Generate Groups"]
-    dropdown = tk.OptionMenu(root, selected_option, *options, command=newWindow)
-    dropdown.pack(pady=20)
+    dropdown = tk.OptionMenu(root, selected_option, *options, command=handleSelection)
+    dropdown.pack(pady=40)
+    
+    #Configuring dropdown size
+    dropdown.grid(row=0,column=0, padx=60, pady=60, sticky="w")
+    dropdown.config(font=font.Font(size=20))
+    menu = dropdown["menu"]
+    menu.config(font=font.Font(size=15))
 
     root.mainloop()
